3c8c0af9baec716bb43537733d5d246b1f0c442c
Douglas Silva <id.oug@hotmail.com>
2024-11-12 13:24:15 -0300

    chore: initial config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,8 +12,37 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        lgBlue: "#009EDD",
+        darkBlue: "#0073A1",
+        green: "#039B00",
+        gray: "#D9D9D9",
+        textColor: "#2F2E41",
+        darkGray: "#999999"
+      },
+      width: {
+        'w-81': '21rem'
+      },
+      fontFamily: {
+        sans: ['Open Sans', 'sans-serif'],
+      },
+      animation: {
+        spin: 'spin 1s linear infinite',
       },
     },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        '.drag-none': {
+          '-webkit-user-drag': 'none',
+          '-khtml-user-drag': 'none',
+          '-moz-user-drag': 'none',
+          '-o-user-drag': 'none',
+          'user-drag': 'none',
+        },
+      });
+    }),
+  ],
+};
+
+export default config;
